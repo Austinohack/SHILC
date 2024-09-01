ba720372a3ef2f2e369dec6bacda56e5cbcfff38
Austinohack <Austinhoang306@gmail.com>
2024-09-01 23:27:28 +0700

    update

--- Website work index.js
+++ Website work index.js
@@ -3,6 +3,6 @@ const navbarItems = document.querySelectorAll('.navbar-item1')
 navbarItems.forEach((item, index) => {
     item.addEventListener('click', function(event) {
         event.preventDefault(); 
-        alert("please login to your account or register")
+        alert("please login to your account or register to use the other page")
     })
 })
